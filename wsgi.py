3f83257f3ebf762ef6ddc9ea681733714ab62338
Tahiry Niaina <tahiry.niaina06@gmail.com>
2025-05-15 19:48:53 +0300

    fixed home endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,11 @@
 from app import create_app
+from flask import render_template, request
 
 app, db = create_app()
+
+@app.route('/', methods=['GET'])
+def home():
+        message = request.args.get('message')
+        if message:
+             return render_template('index.html', message=message)
+        return render_template('index.html')
